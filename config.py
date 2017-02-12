3a37dfba01c68568327ab449c525aae71d82bf5a
Jaume Moral <jaumem@fib.upc.edu>
2017-02-12 16:46:11 +0100

    config.py read values for environment

---
+++ config.py
@@ -0,0 +1,15 @@
+import os
+
+# Set this to False if going to production
+DEBUG = False
+
+# Set this to random value when going to production
+SECRET_KEY = os.environ['secret_key']
+
+# App credentials lazy load configuration
+# Read: https://flask-oauthlib.readthedocs.io/en/latest/client.html#lazy-configuration
+# Get them: https://raco.fib.upc.edu/api/v2/o/
+RACO = {
+    'consumer_key': os.environ['consumer_key'],
+    'consumer_secret': os.environ['consumer_secret']
+}
